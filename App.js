bdf97836f959278365fc39832e9278304fe9e6fc
Saurabh kumar <109866847+Saurabhparshar@users.noreply.github.com>
2023-05-15 08:48:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';.
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUp from './components/register/SignUp';
 import Login from './components/login/Login';
